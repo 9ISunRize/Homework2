9945e9706c4bfc8c5d92cc64d1836b5b24e058e5
Диана <dianamingazova162@gmail.com>
2022-11-30 18:21:11 +0300

    Задвча1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //write your code here
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+    }
+
+}
